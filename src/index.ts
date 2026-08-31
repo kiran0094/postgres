feb1e15921e3d3f3ce12ad22a2480277b58250fd
kiran <Kummkiran@gmail.com>
2026-08-31 18:35:57 +0530

    add expess to the pg

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import {Client} from "pg"
-
+import express from "express";
 const pgClient=new Client("postgresql://neondb_owner:npg_wr9RcQYz0uiv@ep-calm-leaf-ax1o4lki-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+
+const app= express()
+
+app.use(express.json())
+
 
  async function main(){
     await pgClient.connect();
@@ -23,25 +28,21 @@ const pgClient=new Client("postgresql://neondb_owner:npg_wr9RcQYz0uiv@ep-calm-le
 }
 main()
 
-
-
-async function insertData(username: string, email: string, password: string) {
-  const client=new Client("postgresql://neondb_owner:npg_wr9RcQYz0uiv@ep-calm-leaf-ax1o4lki-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-
-
+app.post('/user',async(req,res)=>{
   try {
-    await client.connect(); // Ensure client connection is established
-    // Use parameterized query to prevent SQL injection
+   const {username,email,password}=req.body;
     const insertQuery = "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)";
-    const values = [username, email, password];
-    const res = await client.query(insertQuery, values);
+    const values:string[] = [username, email, password];
+    const res = await pgClient.query(insertQuery, values);
     console.log('Insertion success:', res); // Output insertion result
   } catch (err) {
     console.error('Error during the insertion:', err);
-  } finally {
-    await client.end(); // Close the client connection
-  }
-}
+  } 
+})
+
+app.listen(3000,()=>{
+  console.log("app is lising on port 3000")
+})
 
 // Example usage
 //insertData('username5', 'user5@example.com', 'user_password').catch(console.error);
